ef5e73ed77b5194fbaeb02b62a4357bb5bf15f7e
Joey Honeycutt <JHoneycutt404@Gmail.com>
2019-10-17 13:54:35 -0500

    Detailed mobile stylesheet

--- scripts.js
+++ scripts.js
@@ -3,12 +3,17 @@ window.onscroll = function() {
   let transitionPoint = window.innerHeight;
 
   if (scrollPosY >= transitionPoint / 2) {
+    const buttons = document.getElementsByClassName("navButton");
     // console.log("activate!");
     this.document.getElementById("meBubble").className = "meBubbleTrigger";
     this.document.getElementById("slideBar").className = "slideBarTrigger";
   } else {
     this.document.getElementById("meBubble").className = "";
     this.document.getElementById("slideBar").className = "";
+  }
+
+  if (scrollPosY <= transitionPoint / 2) {
+    this.document.getElementById("buttonContainer").className = "";
   }
 };
 
@@ -20,3 +25,24 @@ window.onscroll = function() {
 //     document.getElementById("myName").innerHTML = "Douglas J Honeycutt";
 //   }
 // }
+
+let toggle = false;
+function mobileMenu() {
+  const buttons = document.getElementById("buttonContainer");
+  console.log(toggle);
+  toggle = !toggle;
+  if (toggle) {
+    buttons.className = "active";
+  } else {
+    buttons.className = "";
+  }
+}
+
+window.addEventListener("click", function(event) {
+  if (
+    !this.document.getElementById("buttonContainer").contains(event.target) &&
+    !this.document.getElementById("menuButton").contains(event.target)
+  ) {
+    this.document.getElementById("buttonContainer").className = "";
+  }
+});
